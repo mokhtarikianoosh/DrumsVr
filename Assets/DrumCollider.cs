d85c2cead7fd4d83e274666414b4542bddd430e3
mokhtarikianoosh <mokhtari@uga.edu>
2017-03-19 04:41:11 -0700

    Drum Colliders

---
+++ Assets/DrumCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c){
+
+		if (c.name == "Snare") {
+			
+			Debug.Log ("Just hit Snare");
+
+		}
+	}
+}
